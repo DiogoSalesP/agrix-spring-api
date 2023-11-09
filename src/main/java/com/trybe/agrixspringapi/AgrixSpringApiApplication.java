1d0e02d53ca1c668d1ed34e5c9e8fcfffe6c884f
Diogo Sales <diogosales.ads@gmail.com>
2023-11-09 14:47:07 -0300

    first commit

---
+++ src/main/java/com/trybe/agrixspringapi/AgrixSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.trybe.agrixspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrixSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrixSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trybe/agrixspringapi/AgrixSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trybe.agrixspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrixSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
